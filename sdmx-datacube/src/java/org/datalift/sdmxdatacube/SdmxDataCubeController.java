8a718a5f6566004c9ce536d0a16ff88a4e7b7126
Thibaud Colas <thibaudcolas@gmail.com>
2013-02-01 17:09:11 +0100

    MVP !

--- sdmx-datacube/src/java/org/datalift/sdmxdatacube/SdmxDataCubeController.java
+++ sdmx-datacube/src/java/org/datalift/sdmxdatacube/SdmxDataCubeController.java
@@ -1,6 +1,6 @@
 /*
  * Copyright / LIRMM 2013
- * Contributor(s) : T. Colas, T. Marmin, F. Scharffe
+ * Contributor(s) : T. Colas, T. Marmin
  *
  * Contact: thibaut.marmin@etud.univ-montp2.fr
  * Contact: thibaud.colas@etud.univ-montp2.fr
@@ -31,3 +31,109 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
+
+package org.datalift.sdmxdatacube;
+
+import java.io.ObjectStreamException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.datalift.fwk.project.Project;
+
+/**
+ *
+ * @version 010213
+ */
+@Path(SDMXDataCubeController.MODULE_NAME)
+public class SDMXDataCubeController extends ModuleController {
+    //-------------------------------------------------------------------------
+    // Constants
+    //-------------------------------------------------------------------------
+
+    /** The module's name. */
+    public static final String MODULE_NAME = "sdmxdatacube";
+
+    //-------------------------------------------------------------------------
+    // Instance members
+    //-------------------------------------------------------------------------
+
+    //-------------------------------------------------------------------------
+    // Constructors
+    //-------------------------------------------------------------------------
+
+    /**
+     * Creates a new InterconnectionController instance.
+     */
+    public SDMXDataCubeController() {
+        //TODO Switch to the right position.
+        super(MODULE_NAME, 13371337);
+
+        label = getTranslatedResource(MODULE_NAME + ".button");
+    }
+
+    //-------------------------------------------------------------------------
+    // Project management
+    //-------------------------------------------------------------------------
+
+    /**
+     * Tells the project manager to add a new button to projects with at least
+     * two sources.
+     * @param p Our current project.
+     * @return The URI to our project's main page.
+     */
+    public final UriDesc canHandle(Project p) {
+        UriDesc uridesc = null;
+
+        try {
+            // The project can be handled if it has at least one RDF source.
+            if (true) {
+              // link URL, link label
+                uridesc = new UriDesc(this.getName() + "?project=" + p.getUri(), this.label);
+
+                if (this.position > 0) {
+                    uridesc.setPosition(this.position);
+                }
+                LOG.debug("Project {} can use SDMXToDataCube", p.getTitle());
+            }
+            else {
+                LOG.debug("Project {} can not use SDMXToDataCube", p.getTitle());
+            }
+        }
+        catch (URISyntaxException e) {
+            LOG.fatal("Uh!", e);
+            throw new RuntimeException(e);
+        }
+        return uridesc;
+    }
+
+    //-------------------------------------------------------------------------
+    // Web services
+    //-------------------------------------------------------------------------
+
+    /**
+     * Index page handler of the SDMXToDataCube module.
+     * @param projectId the project using SDMXToDataCube
+     * @return Our module's interface.
+     * @throws ObjectStreamException
+     */
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public Response getIndexPage(@QueryParam("project") URI projectId) throws ObjectStreamException {
+        // Retrieve the current project and its sources.
+        Project proj = this.getProject(projectId);
+
+        HashMap<String, Object> args = new HashMap<String, Object>();
+        args.put("it", proj);
+
+        return Response.ok(this.newViewable("/sdmxdatacube.vm", args)).build();
+    }
+}
