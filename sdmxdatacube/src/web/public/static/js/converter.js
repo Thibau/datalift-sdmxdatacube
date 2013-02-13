66412b64849711277fac620a6240045b3ad77f13
Thibaud Colas <thibaudcolas@gmail.com>
2013-02-14 00:46:11 +0100

    Data bindings now work properly

--- sdmxdatacube/src/web/public/static/js/converter.js
+++ sdmxdatacube/src/web/public/static/js/converter.js
@@ -2,12 +2,30 @@ function ConverterViewModel(projectSources, viewResults) {
   var self = this;
 
   self.projectSources = projectSources;
-  self.inputSource = ko.observable();
 
-  self.outputSourceName = ko.observable();
-  self.outputSourceURI = ko.observable();
 
-  self.uriFormat = ko.observable();
+  self.inputSource = ko.observable(self.projectSources[0]['uri']);
+
+  self.outputSourceTitle = ko.computed(function() {
+    return findValue(self.projectSources, self.inputSource(), 'outputTitle');
+  });
+
+  self.outputSourceURI = ko.computed(function() {
+    return findValue(self.projectSources, self.inputSource(), 'outputURI');
+  });
+
+  self.uriPattern = ko.observable(self.projectSources[0]['uriPattern']);
 
   self.viewResults = ko.observable(viewResults);
+}
+
+function findValue(array, uri, property) {
+  var i = 0;
+  var found = false;
+  while (!found && i < array.length) {
+    found = array[i]['uri'] === uri;
+    i++;
+  }
+
+  return found ? array[i - 1][property] : '';
 }
