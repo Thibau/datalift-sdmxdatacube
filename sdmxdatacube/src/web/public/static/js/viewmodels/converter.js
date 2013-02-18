728df3337f3dec5b98eef8c2e8f8909607bd91d9
Thibaud Colas <thibaudcolas@gmail.com>
2013-02-18 14:40:09 +0100

    Refactor + comment code

--- sdmxdatacube/src/web/public/static/js/viewmodels/converter.js
+++ sdmxdatacube/src/web/public/static/js/viewmodels/converter.js
@@ -21,27 +21,13 @@ define([
     var self = this;
 
     // Transform our array of Objects to an array of Sources.
-    self.defaultSources = ko.observableArray(defaultSources.map(function (elt) {
+    self.defaultSources = defaultSources.map(function (elt) {
       return new Source(elt.parent, elt.title, elt.uri, elt.uriPattern, elt.creator, elt.project);
-    }));
+    });
 
     self.currentSource = ko.observable(currentSource);
 
     self.viewResults = ko.observable(viewResults);
-
-    // Internal computed observable that fires whenever anything changes.
-    ko.computed(function() {
-      // Store a clean copy to local storage.
-      localStorage.setItem(g.localStorageCurrentSource, ko.toJSON(self.currentSource()));
-    }).extend({
-       // Save at most twice per second.
-      throttle: 500
-    });
-
-    self.resetValues = function() {
-      window.console.log('reset');
-      localStorage.removeItem(g.localStorageCurrentSource);
-    };
 
     self.launchConverter = function(data, event) {
       window.console.log('launch');
@@ -63,6 +49,20 @@ define([
       });
     };
 
+    self.resetValues = function() {
+      window.console.log('reset');
+      //TODO self.currentSource(self.defaultSources[0]);
+      localStorage.removeItem(g.localStorageCurrentSource);
+    };
+
+    // Internal computed observable that fires whenever anything changes.
+    ko.computed(function() {
+      // Store a clean copy to local storage.
+      localStorage.setItem(g.localStorageCurrentSource, ko.toJSON(self.currentSource()));
+    }).extend({
+       // Save at most twice per second.
+      throttle: 500
+    });
   };
   return ViewModel;
 });
