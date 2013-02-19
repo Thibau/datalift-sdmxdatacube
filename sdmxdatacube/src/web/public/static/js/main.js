9b165556490604b0017e1d25ad0962667beee4dd
Thibaud Colas <thibaudcolas@gmail.com>
2013-02-19 08:58:34 +0100

    Added translation of client-side validation errors

--- sdmxdatacube/src/web/public/static/js/main.js
+++ sdmxdatacube/src/web/public/static/js/main.js
@@ -18,15 +18,16 @@ require.config({
 require([
   'knockout',
   'config/global',
+  'config/translation',
   'viewmodels/converter',
   'extends/handlers',
   'extends/native',
   'validation'
-], function(ko, g, ConverterViewModel, validation){
+], function(ko, g, i18n, ConverterViewModel, validation){
   /*
     Here it is time for some explanation.
     RequireJS modules define their dependencies explicitly,
-    and we should here define a dependency to a config/defaults module
+    and we should here define a dependency to a config/parameters module
     which could contain the default values for our form, put there by Velocity (/ AJAX).
     But Datalift does not widely use RequireJS nor Knockout at the moment,
     so this isn't that bad to use such a half-assed solution.
@@ -34,14 +35,19 @@ require([
     This is a "temporary" fix.
    */
   //'use strict';
-  var defaults = inlineDefaults;
+  var parameters = inlineParameters;
 
   // Configure the validation to use parameters defined in global.
   ko.validation.configure(g.validationParameters);
+
+  // Translate validation to current language.
+  if (parameters.language === 'fr') {
+    ko.validation.localize(i18n.validation[parameters.language]);
+  }
 
   // Check local storage for values.
   var currentSource = ko.utils.parseJson(localStorage.getItem(g.localStorageCurrentSource));
 
   // Bind a new instance of our view model to the page.
-  ko.applyBindings( new ConverterViewModel(defaults.sources, currentSource || null, defaults.viewResults));
+  ko.applyBindings( new ConverterViewModel(parameters.defaults.sources, currentSource || null, parameters.defaults.viewResults));
 });
